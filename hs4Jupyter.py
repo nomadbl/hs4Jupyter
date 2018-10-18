8339e4c887dbefe960ebca55c6cf7a438abea305
Lior Blech <nomadbl@gmail.com>
2018-10-18 18:43:41 +0300

    incomplete commit, get back home, learning machinery of Kernel class and subprocess

--- hs4Jupyter.py
+++ hs4Jupyter.py
@@ -2,6 +2,7 @@ from ipykernel.kernelbase import Kernel
 import subprocess
 
 class Hs4Jupyter(Kernel):
+    $ automate some of these in future
     implementation = 'GHC'
     implementation_version = '8.4'
     language = 'Haskell'
@@ -12,11 +13,23 @@ class Hs4Jupyter(Kernel):
         'file_extension': '.hs',
     }
     banner = "GHC Haskell kernel for Jupyter notebook"
+    
+    # this is called by IPKernelApp.launch_instance. initialize the interpreter
+    def __init__(self, **kwargs):
+        Kernel.__init__(self, **kwargs)
+        self._start_Haskell()
+
+    def _start_Haskell(self):
+        
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
-        if not silent:
-	    stream_content = {'name': 'stdout', 'text': code}
+        # run code using the PIPE
+    	if not silent:
+	    #code = str.rstrip(str.rstrip(code)) $ strip string of useless whitespace. (necessary?)
+	    output = 'kernel stdout here'
+	    # send the output to stdout
+	    stream_content = {'name': 'stdout', 'text': output}
 	    # send_response is used for printing to the stdio (notebook)
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
@@ -28,6 +41,10 @@ class Hs4Jupyter(Kernel):
 		# results of the computation
                 'user_expressions': {},
                }
+
+    def do_shutdown(restart):
+	    # orderly shutdown of the kernel
+	    return
 
 if __name__ == '__main__':
     from ipykernel.kernelapp import IPKernelApp
